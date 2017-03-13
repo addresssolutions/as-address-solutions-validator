2cd2762db782de19ac9435950bd232df0188586c
addresssolutions <r.geerken@address-solutions.de>
2017-03-13 16:46:30 +0100

    initial chekin

---
+++ as_validate_sample.js
@@ -0,0 +1,32 @@
+// use the address solutions library
+var as_address_solutions_validator = require('./dist/as-address-solutions-validator.js');
+
+function call_as_validate_salutation(anrede) {
+  var as_req = new as_address_solutions_validator.ASValidateNameRequest(
+    1,                                  // VersionInput
+    "de",                               // Countrycode
+    anrede);                            // Anrede, die validiert werden soll
+
+  as_address_solutions_validator.ASValidateSalutation(as_req, function (error, response) {
+    if (error) {
+      console.log("error response von ASValidateName: " + error.ErrorStatus + "/" + error.ErrorMessage);
+    }
+    else {
+      // following elements are delivered in response (cf as-address-solutions-validator.js)
+      // ReleaseInfo: ReleaseInfo of used Server
+      // GeneralStatus: general Status (tbd)
+      // ValidationCode: Validation Code 1: Element ok, 2: Not OK, but could stay here, 3: not OK should be moved
+      // ValidationMessage: Message zur Validierung
+      console.log('');
+      console.log('==============================================');
+      console.log('ReleaseInfo            : '+ response.ReleaseInfo);
+      console.log('GeneralStatus          : '+ response.GeneralStatus);
+      console.log('ValidationCode         : '+ response.ValidationCode);
+      console.log('ValidationMessage      : '+ response.ValidationMessage);
+      console.log('==============================================');
+      console.log('');
+    }
+  });
+}
+
+call_as_validate_salutation("Dr.");
